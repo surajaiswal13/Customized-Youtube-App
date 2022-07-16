82efd2ccac42ca47253fa5a76411e8885c32c70f
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-07-17 02:45:36 +0530

    [Update] YTData Model added

--- core/models.py
+++ core/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class YTData(models.Model):
+
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=200)
+    published_at = models.DateTimeField()
+    thumbnail_url_default = models.URLField(max_length=100)
+    thumbnail_url_medium = models.URLField(max_length=100)
+    thumbnail_url_high = models.URLField(max_length=100)
