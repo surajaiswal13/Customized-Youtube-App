e57ef5b4027a15d6fc6543107ebed56c28f460f6
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-07-17 19:23:41 +0530

    [Update] Search API with all requirements

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from core.viewsets import YTDataViewSet
+from core.views import YTDataSearchAPIView
 
 
 urlpatterns = [
-
+    path('search/', YTDataSearchAPIView.as_view(), name='search')
 ]
 
 router = DefaultRouter()
--- core/views.py
+++ core/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from core.models import YTData
+
 
 # Create your views here.
+
+class YTDataSearchAPIView(APIView):
+    '''
+    ApiView for Searching based on Title and
+        Description
+    '''
+
+    def post(self, request):
+
+        title = request.data.get('title')
+        description = request.data.get('description')
+
+        result = YTData.objects.filter(title__icontains=title, description__icontains=description)
+
+        response_list = [
+            {
+                "id": item.id,
+                "title": item.title,
+                "description": item.description,
+                "published_at": item.published_at,
+                "thumbnail_url_default": item.thumbnail_url_default,
+                "thumbnail_url_medium": item.thumbnail_url_medium,
+                "thumbnail_url_high": item.thumbnail_url_high
+            }
+
+            for item in result
+        ]
+
+        return Response(response_list)
